f40f3abd1be649996b303246af0fe80c01c8f971
kalantyr <kalantyr@yandex.ru>
2021-05-17 21:31:41 +0300

    Урок 3 - хэштаблицы

--- lesson3/src/main/java/ru/kalantyr/java/lesson3/MainApp.java
+++ lesson3/src/main/java/ru/kalantyr/java/lesson3/MainApp.java
@@ -1,4 +1,71 @@
 package ru.kalantyr.java.lesson3;
 
+import java.util.*;
+
 public class MainApp {
+
+    /**
+     * 1. Написать метод, который меняет два элемента массива местами (массив может быть любого ссылочного типа)
+     */
+    public static <T extends Object> void swap(T[] array, int i1, int i2)
+            throws ArrayIndexOutOfBoundsException, IllegalArgumentException
+    {
+        if (array == null)
+            throw new IllegalArgumentException("array");
+
+        if (i1 == i2)
+            return;
+
+        var v1 = array[i1];
+        array[i1] = array[i2];
+        array[i2] = v1;
+    }
+
+    /**
+     * 2. Написать метод, который преобразует массив в ArrayList
+     */
+    public static <T> ArrayList<T> arrayToList(T[] array) throws IllegalArgumentException {
+        if (array == null)
+            throw new IllegalArgumentException("array");
+
+        var list = new ArrayList<T>(array.length);
+        for (var i: array)
+            list.add(i);
+        return list;
+    }
+
+    public static void main(String[] args) {
+
+        // 4. Создать массив с набором слов.
+        // Найти и вывести список слов, из которых состоит массив.
+        // Посчитать, сколько раз встречается каждое слово.
+        String[] values = {
+                "Яблоко", "Апельсин", "Груша", "Яблоко", "Яблоко", "Апельсин", "Банан", "Груша", "Яблоко", "Яблоко"
+        };
+        var map = countWords(values);
+        for (var entry: map.entrySet())
+            System.out.printf("%s (%d)%n", entry.getKey(), entry.getValue());
+
+        // ---------- output ----------
+        // Груша (2)
+        // Яблоко (5)
+        // Апельсин (2)
+        // Банан (1)
+    }
+
+    /**
+     * Подсчитывает слова
+     */
+    public static Map<String, Integer> countWords(String[] values) throws IllegalArgumentException {
+        if (values == null)
+            throw new IllegalArgumentException("array");
+
+        var map = new HashMap<String, Integer>();
+        for (var s : values)
+            if (map.containsKey(s))
+                map.put(s, map.get(s) + 1);
+            else
+                map.put(s, 1);
+        return map;
+    }
 }
--- lesson3/src/test/java/ru/kalantyr/java/lesson3/tests/MainAppTests.java
+++ lesson3/src/test/java/ru/kalantyr/java/lesson3/tests/MainAppTests.java
@@ -1,6 +1,44 @@
 package ru.kalantyr.java.lesson3.tests;
 
+import org.junit.Assert;
 import org.junit.Test;
+import ru.kalantyr.java.lesson3.MainApp;
+
+import java.util.ArrayList;
 
 public class MainAppTests {
+
+    @Test
+    public void swap_test(){
+        var strings = new String[] { "A", "B", "C" };
+        MainApp.swap(strings, 0, 2);
+        Assert.assertEquals("C", strings[0]);
+        Assert.assertEquals("A", strings[2]);
+
+        MainApp.swap(strings, 1, 1);
+        Assert.assertEquals("B", strings[1]);
+
+        var floats = new Float[] { 1.1f, 2.2f, 3.3f };
+        MainApp.swap(floats, 0, 2);
+        Assert.assertEquals(3.3f, floats[0], 0.01f);
+        Assert.assertEquals(1.1f, floats[2], 0.01f);
+    }
+
+    @Test
+    public void arrayToList_test() {
+        String[] array = { "Aa", "Bb", "Cc" };
+        var list = MainApp.arrayToList(array);
+        Assert.assertEquals(ArrayList.class, list.getClass());
+        Assert.assertEquals(3, list.size());
+    }
+
+    @Test
+    public void countWords_test() {
+        String[] values = {
+                "Яблоко", "Апельсин", "Груша", "Яблоко", "Яблоко", "Апельсин"
+        };
+        var map = MainApp.countWords(values);
+        Assert.assertEquals(3, (long) map.get("Яблоко"));
+        Assert.assertEquals(2, (long) map.get("Апельсин"));
+    }
 }
