f51e88dcf4b3022f1f69b500f76a03d11caf0fd5
kalantyr <kalantyr@yandex.ru>
2021-05-24 18:15:42 +0300

    lesson 5

---
+++ lesson5/src/main/java/ru/kalantyr/java/lesson5/MainApp.java
@@ -0,0 +1,6 @@
+package ru.kalantyr.java.lesson5;
+
+public class MainApp {
+    public static void main(String[] args){
+    }
+}
---
+++ lesson5/src/test/java/ru/kalantyr/java/lesson5/tests/MainAppTests.java
@@ -0,0 +1,6 @@
+package ru.kalantyr.java.lesson5.tests;
+
+import org.junit.Test;
+
+public class MainAppTests {
+}
