ceda53b5ba41a48900958d5e627740650acbd5fd
kalantyr <kalantyr@yandex.ru>
2021-05-27 23:03:30 +0300

    lesson 6

---
+++ lesson6/src/main/java/ru/kalantyr/java/lesson6/MainApp.java
@@ -0,0 +1,6 @@
+package ru.kalantyr.java.lesson6;
+
+public class MainApp {
+    public static void main(String[] args){
+    }
+}
---
+++ lesson6/src/test/java/ru/kalantyr/java/lesson6/tests/MainAppTests.java
@@ -0,0 +1,6 @@
+package ru.kalantyr.java.lesson6.tests;
+
+import org.junit.Test;
+
+public class MainAppTests {
+}
