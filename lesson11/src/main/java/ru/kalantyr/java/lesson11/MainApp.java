13e94b69cf1a49a10f1eaa366e55668757e19475
kalantyr <kalantyr@yandex.ru>
2021-06-21 20:51:52 +0300

    1

--- lesson11/src/main/java/ru/kalantyr/java/lesson11/MainApp.java
+++
@@ -1,6 +0,0 @@
-package ru.kalantyr.java.lesson11;
-
-public class MainApp {
-    public static void main(String[] args){
-    }
-}
--- lesson11/src/test/java/ru/kalantyr/java/lesson11/tests/MainAppTests.java
+++
@@ -1,6 +0,0 @@
-package ru.kalantyr.java.lesson11.tests;
-
-import org.junit.Test;
-
-public class MainAppTests {
-}
